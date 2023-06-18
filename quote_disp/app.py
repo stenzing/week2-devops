c123e818030665004d01aac3e574055f03daed03
Gabor Steninger <gabor.stenzinger@gmail.com>
2023-06-18 17:56:14 +0200

    Week 2 assignments

--- quote_disp/app.py
+++ quote_disp/app.py
@@ -3,26 +3,28 @@ import random
 import requests
 from flask import Flask, render_template
 
-app = Flask(__name__)
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route("/health")
+    def health():
+        return "healthy"
+
+    @app.route("/")
+    def home():
+        return render_template("index.html")
 
 
-@app.route("/health")
-def health():
-    return "healthy"
+    @app.route("/get_quote")
+    def quote():
+        quote = requests.get("http://gen:5000/quote").text
+        print("quote - ", quote)
 
+        return render_template("quote.html", quote=quote)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-
-@app.route("/get_quote")
-def quote():
-    quote = requests.get("http://gen:5000/quote").text
-    print("quote - ", quote)
-
-    return render_template("quote.html", quote=quote)
+    return app
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=5001)
+    create_app().run(host="0.0.0.0", debug=True, port=5001)
